6fe7738326a4b11c4849bebb3f38fb92880bb4df
Faiz Shaikh <faiz.shaikh@statravel.com>
2020-06-27 14:54:32 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
-require(`dotenv`).config()
+require(`dotenv`).config();
 
 module.exports = {
   siteMetadata: {
-    title: "Reflex",
-    description: "Starter for the Reflex.",
+    title: "Marhaba International",
+    description: "Tours and Travels",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
   plugins: ["@reflexjs/gatsby-theme-base"],
-}
+};
